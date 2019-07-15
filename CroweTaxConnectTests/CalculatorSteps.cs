3dd7e31facb86bec77a12686031374a7a8e84727
Ibrahim Ahmed <ibrahim.ahmed@crowe.com>
2019-07-15 15:03:10 -0400

    Initial commit of barebones project.

---
+++ CroweTaxConnectTests/CalculatorSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace CroweTaxConnectTests.bin
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+	        return;
+        }
+        
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int p0)
+        {
+	        return;
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+	        return;
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+	        return;
+        }
+    }
+}
